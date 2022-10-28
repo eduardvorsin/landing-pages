aca0ecf8301e934442a27a113e15f67ba975f8e7
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-10-28 09:38:46 +0600

    [test] added UI tests for the calculator

--- pet-projects/Calculator/js/index.test.js
+++ pet-projects/Calculator/js/index.test.js
@@ -61,3 +61,158 @@ function calculatorUISetup(keydownHandler, pointerdownHandler) {
   screen.getByRole('textbox').addEventListener('keydown', keydownHandler);
   screen.getByTestId('calculator').addEventListener('pointerdown', (e) => { pointerdownHandler(e, screen.getByRole('textbox')) });
 }
+
+describe('Calculator UI tests', () => {
+  const pointerdownHandler = jest.fn(originalPointerdownHandler);
+  const keydownHandler = jest.fn(originalKeydownHandler);
+
+  const numberKeys = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '.',
+  ];
+  const invalidKeys = ['a', '#', 'B', ',', '\\', '!', ' '];
+  const allCalculatorPad = [
+    ...numberKeys,
+    'C',
+    '+/-',
+    '%',
+    '/',
+    '*',
+    '-',
+    '+',
+    '√',
+    '=',
+  ];
+
+  beforeAll(() => {
+    calculatorUISetup(keydownHandler, pointerdownHandler);
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
+
+  afterEach(() => {
+    screen.getByRole('textbox').value = '';
+  });
+
+  test.each(allCalculatorPad)('pressing the %s key starts the pointerdown handler', async (value) => {
+    await userEvent.click(screen.getByText(value));
+    expect(pointerdownHandler).toHaveBeenCalledTimes(1);
+  })
+
+  test('entering any value triggers the keydown handler', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('2 r');
+    expect(keydownHandler).toHaveBeenCalledTimes(3);
+  })
+
+  test.each(numberKeys)('Pressing a valid numeric key %s', async (value) => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard(value);
+    expect(screen.getByRole('textbox')).toHaveDisplayValue(value);
+  })
+
+  test.each(invalidKeys)('Pressing an invalid %s key', async (value) => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard(value);
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('');
+  });
+
+  test('clicking on the C button sets any value to 0', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('2');
+    await userEvent.click(screen.getByText('C'));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('0');
+  });
+
+  test('clicking on the negation button makes any positive number negative', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('5');
+    await userEvent.click(screen.getByText('+/-'));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('-5');
+    await userEvent.click(screen.getByText('+/-'));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('5');
+  });
+
+  test('clicking on the negation button does leaves the value 0 the same', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('0');
+    await userEvent.click(screen.getByText('+/-'));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('0');
+  });
+
+  test('clicking on a percentage игеещт returns 1% of 100% for a given number', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('56');
+    await userEvent.click(screen.getByText('%'));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('0.56');
+  });
+
+  test('clicking on the root button returns the square root of this number', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('64');
+    await userEvent.click(screen.getByText('√'));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('8');
+  });
+
+  test('the sum of two numbers', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('5');
+    await userEvent.click(screen.getByText('+'));
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('16');
+    await userEvent.click(screen.getByText('+/-'));
+    await userEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('-11');
+  });
+
+  test('the difference between two numbers', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('55');
+    await userEvent.click(screen.getByText('-'));
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('15');
+    await userEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('40');
+  });
+
+  test('multiplication of two numbers', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('9');
+    await userEvent.click(screen.getByText('*'));
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('8');
+    await userEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('72');
+  });
+
+  test('dividing two numbers', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('36');
+    await userEvent.click(screen.getByText('/'));
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('2');
+    await userEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue('18');
+  });
+
+  test('division by zero', async () => {
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('99');
+    await userEvent.click(screen.getByText('/'));
+    await userEvent.click(screen.getByRole('textbox'));
+    await userEvent.keyboard('0');
+    await userEvent.click(screen.getByText('='));
+    expect(screen.getByRole('textbox')).toHaveDisplayValue(/division by 0 error/i);
+  });
+})
