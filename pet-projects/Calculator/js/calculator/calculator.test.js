808c6cde365b51e24ae705ed247c32bbb41200bc
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-10-28 09:33:42 +0600

    [test] added makeOperation tests

--- pet-projects/Calculator/js/calculator/calculator.test.js
+++ pet-projects/Calculator/js/calculator/calculator.test.js
@@ -37,3 +37,60 @@ describe('updateInput tests', () => {
     expect(updateInput('1', undefined)).toBeUndefined();
   });
 });
+
+describe('makeOperation tests', () => {
+  beforeAll(() => {
+    document.body.innerHTML =
+      '<input class="calculator__input" type="text">';
+  });
+
+  const arithmeticOperations = [
+    '+',
+    '-',
+    '/',
+    '*',
+  ];
+
+  afterEach(() => {
+    screen.getByRole('textbox').value = '';
+  });
+
+  test('the input value is not a number and the passed value is not a cleanup operation', () => {
+    const input = screen.getByRole('textbox');
+    expect(makeOperation('abc', input)).toBeUndefined();
+  });
+
+  test('the passed value is cleanup operation', () => {
+    const input = screen.getByRole('textbox');
+    makeOperation('clear', input);
+    expect(input).toHaveDisplayValue('0');
+  });
+
+  test('the passed value is negation operation', () => {
+    const input = screen.getByRole('textbox');
+    updateInput('5', input);
+    makeOperation('negation', input);
+    expect(input).toHaveDisplayValue('-5');
+  });
+
+  test.each(arithmeticOperations)('the passed value is a %s operation', (value) => {
+    const input = screen.getByRole('textbox');
+    updateInput('-1', input);
+    makeOperation(value, input);
+    expect(input).toHaveDisplayValue('');
+  });
+
+  test('the passed value is a % operation', () => {
+    const input = screen.getByRole('textbox');
+    updateInput('100', input);
+    makeOperation('%', input);
+    expect(input).toHaveDisplayValue('1');
+  });
+
+  test('the passed value is a square root operation', () => {
+    const input = screen.getByRole('textbox');
+    updateInput('25', input);
+    makeOperation('sqrt', input);
+    expect(input).toHaveDisplayValue('5');
+  });
+});
