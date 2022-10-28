37ebd64e0eda3c9c52516c92e72a58f7e6b282d4
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-10-28 10:01:58 +0600

    [feature] added sound when pressing the calculator buttons

--- pet-projects/Calculator/js/index.js
+++ pet-projects/Calculator/js/index.js
@@ -6,5 +6,10 @@ const calculatorInput = calculator.querySelector('.calculator__input');
 
 calculatorInput.addEventListener('keydown', keydownHandler);
 calculator.addEventListener('pointerdown', (e) => {
+  if (e.target.tagName === 'BUTTON') playSound();
+
   pointerdownHandler(e, calculatorInput);
 });
+
+const beepSound = new Audio('../../audio/beep-sound.mp3');
+export const playSound = () => beepSound.play();
