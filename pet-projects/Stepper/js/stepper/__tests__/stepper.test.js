37df77d73d8a31ef06c93ae873cfafff20671e39
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-08 20:04:13 +0600

    [fix] rearranged the role of the desired element

--- pet-projects/Stepper/js/stepper/__tests__/stepper.test.js
+++ pet-projects/Stepper/js/stepper/__tests__/stepper.test.js
@@ -112,12 +112,12 @@ function stepperUISetup(options = {
   const { step = 1, min = 1, max = 10, value = 2 } = options;
 
   document.body.innerHTML =
-    `<div class="stepper">
+    `<div class="stepper" role="spinbutton">
     <label class="visually-hidden stepper__label" for="stepper">Spinbuttton</label>
     <button class="stepper__btn stepper__decrease" data-stepper-decrease>
       decrease
     </button>
-    <input class="stepper__input" role="spinbutton" id="stepper" type="text" inputmode="numeric"
+    <input class="stepper__input" id="stepper" type="text" inputmode="numeric"
     data-stepper-step="${step}" data-stepper-min="${min}" data-stepper-max="${max}" value="${value}" placeholder="number" pattern="\d"
     autocomplete="off">
     <button class="stepper__btn stepper__increase" data-stepper-increase>
