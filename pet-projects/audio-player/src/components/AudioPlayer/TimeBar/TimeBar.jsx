61611e19e6e58a57858fc893add2c1b2b37ef569
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-16 19:59:40 +0600

    [feature] added TimeBar component

---
+++ pet-projects/audio-player/src/components/AudioPlayer/TimeBar/TimeBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { StyledTimeBar } from "./StyledTimeBar";
+
+export const TimeBar = ({
+	currentTime,
+	duration,
+	className,
+	...props
+}) => {
+	return (
+		<StyledTimeBar
+			className={className}
+			{...props}
+		>
+			{currentTime}/{duration}
+		</StyledTimeBar>
+	);
+};
+
+
+TimeBar.propTypes = {
+	currentTime: PropTypes.string,
+	duration: PropTypes.string,
+}
+
+TimeBar.defaultProps = {
+	currentTime: '00:00',
+	duration: '00:00',
+}
