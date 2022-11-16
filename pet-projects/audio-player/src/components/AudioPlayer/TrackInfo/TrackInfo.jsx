af404f638fbab55163c9a4384f0d998ca1029a94
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-16 20:00:24 +0600

    [feature] added TrackInfo component

---
+++ pet-projects/audio-player/src/components/AudioPlayer/TrackInfo/TrackInfo.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { memo } from 'react';
+import { StyledTrackArtist, StyledTrackInfo, StyledTrackName } from "./StyledTrackInfo";
+
+export const TrackInfo = memo(({
+	trackName,
+	trackArtist,
+	className,
+}) => {
+	return (
+		<StyledTrackInfo className={className}>
+			<StyledTrackName>{trackName}</StyledTrackName>
+			<StyledTrackArtist>{trackArtist}</StyledTrackArtist>
+		</StyledTrackInfo>
+	);
+});
+
+TrackInfo.propTypes = {
+	trackName: PropTypes.string,
+	trackArtist: PropTypes.string,
+	className: PropTypes.string,
+}
+
+TrackInfo.defaultProps = {
+	trackName: '',
+	trackArtist: '',
+	className: '',
+}
