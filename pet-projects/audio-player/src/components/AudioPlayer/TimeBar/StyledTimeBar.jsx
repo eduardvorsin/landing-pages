3e18c98845be25fa9f5a944a2a881353e1e4d062
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-16 19:59:22 +0600

    [feature] added StyledTimeBar component

---
+++ pet-projects/audio-player/src/components/AudioPlayer/TimeBar/StyledTimeBar.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledTimeBar = styled.time`
+	font-size: 16px;
+	letter-spacing: 0.3px;
+	margin-bottom: 10px;
+	align-self: flex-end;
+	color: var(--secondary-color);
+`;
