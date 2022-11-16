1eef5303ded8cd7227f54dac0ccd7aa2ac36ef7b
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-16 19:58:57 +0600

    [feature] added ProgressRangeSlider component

---
+++ pet-projects/audio-player/src/components/AudioPlayer/ProgressRangeSlider/ProgressRangeSlider.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { RangeSlider } from "../../UI/RangeSlider/RangeSlider";
+
+export const ProgressRangeSlider = styled(RangeSlider)`
+	margin-bottom: 30px;
+`;
+
