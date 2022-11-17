c8c25e2160073c9ba856ac0cf8281deccacebc6d
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-17 18:02:51 +0600

    [feature] added action types

---
+++ pet-projects/audio-player/src/actions/actions_types.js
@@ -0,0 +1,11 @@
+export const CHANGE_DURATION = 'change-duration';
+export const CHANGE_CURRENT_TIME = 'change-current-time';
+export const CHANGE_VOLUME = 'change-volume';
+export const CHANGE_PLAYBACKRATE = 'change-playback-rate';
+export const TOGGLE_LOOPING = 'toggle-looping';
+export const MUTE = 'mute';
+export const UNMUTE = 'unmute';
+export const PLAY = 'play';
+export const PAUSE = 'pause';
+export const LOOP = 'loop';
+export const UNLOOP = 'unloop';
