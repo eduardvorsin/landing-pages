e6801fd6bcf517cea456dbb3a7ec14aa660c18aa
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-11-17 18:05:37 +0600

    [feature] added StyledAudioPlayer component

---
+++ pet-projects/audio-player/src/components/AudioPlayer/StyledAudioPlayer.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledAudioPlayer = styled.div`
+	background: var(--player-background-gradient);
+	display: flex;
+	flex-direction: column;
+	max-width: 330px;
+	width:100%;
+	padding: 15px;
+`;
