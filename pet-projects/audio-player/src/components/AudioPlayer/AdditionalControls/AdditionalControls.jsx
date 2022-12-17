00c8c509aa0dcae8ed3d4f1c217bfda52f0de3d6
eduardvorsin <eduardvorsin2019@yandex.kz>
2022-12-17 19:02:58 +0600

    spaces have been updated

--- pet-projects/audio-player/src/components/AudioPlayer/AdditionalControls/AdditionalControls.jsx
+++ pet-projects/audio-player/src/components/AudioPlayer/AdditionalControls/AdditionalControls.jsx
@@ -8,69 +8,69 @@ import { ReactComponent as NoRepeatIcon } from '../../../assets/images/icons/no-
 import { ReactComponent as DownloadIcon } from '../../../assets/images/icons/download.svg';
 
 export const AdditionalControls = React.memo(({
-	showDownloadControl,
-	downloadLink,
-	showPlaybackRateControl,
-	changePlaybackRate,
-	playbackRate,
-	showLoopControl,
-	changeLooping,
-	isLooped,
+  showDownloadControl,
+  downloadLink,
+  showPlaybackRateControl,
+  changePlaybackRate,
+  playbackRate,
+  showLoopControl,
+  changeLooping,
+  isLooped,
 }) => {
 
-	const repeatButtonIcon = isLooped ? <RepeatIcon /> : <NoRepeatIcon />;
+  const repeatButtonIcon = isLooped ? <RepeatIcon /> : <NoRepeatIcon />;
 
-	return (
-		<StyledAdditionalControls>
-			{showDownloadControl &&
-				<Button
-					withoutVisibleText
-					href={downloadLink}
-					startIcon={<DownloadIcon />}
-					download
-				>
-					download track
-				</Button>
-			}
-			{showPlaybackRateControl &&
-				<PlaybackRateButton
-					aria-label='current playback rate'
-					onClick={changePlaybackRate}
-				>
-					{`${playbackRate}x`}
-				</PlaybackRateButton>
-			}
-			{showLoopControl &&
-				<Button
-					withoutVisibleText
-					startIcon={repeatButtonIcon}
-					onClick={changeLooping}
-				>
-					{isLooped ? 'don\'t repeat the song ' : 'repeat song infinite'}
-				</Button>
-			}
-		</StyledAdditionalControls>
-	);
+  return (
+    <StyledAdditionalControls>
+      {showDownloadControl &&
+        <Button
+          withoutVisibleText
+          href={downloadLink}
+          startIcon={<DownloadIcon />}
+          download
+        >
+          download track
+        </Button>
+      }
+      {showPlaybackRateControl &&
+        <PlaybackRateButton
+          aria-label='current playback rate'
+          onClick={changePlaybackRate}
+        >
+          {`${playbackRate}x`}
+        </PlaybackRateButton>
+      }
+      {showLoopControl &&
+        <Button
+          withoutVisibleText
+          startIcon={repeatButtonIcon}
+          onClick={changeLooping}
+        >
+          {isLooped ? 'don\'t repeat the song' : 'repeat song infinite'}
+        </Button>
+      }
+    </StyledAdditionalControls>
+  );
 });
 
 AdditionalControls.propTypes = {
-	showDownloadControl: PropTypes.bool,
-	downloadLink: PropTypes.string,
-	showPlaybackRateControl: PropTypes.bool,
-	changePlaybackRate: PropTypes.func,
-	playbackRate: PropTypes.number,
-	showLoopControl: PropTypes.bool,
-	changeLooping: PropTypes.func,
-	isLooped: PropTypes.bool,
+  showDownloadControl: PropTypes.bool,
+  downloadLink: PropTypes.string,
+  showPlaybackRateControl: PropTypes.bool,
+  changePlaybackRate: PropTypes.func,
+  playbackRate: PropTypes.number,
+  showLoopControl: PropTypes.bool,
+  changeLooping: PropTypes.func,
+  isLooped: PropTypes.bool,
 };
 
 AdditionalControls.defaultProps = {
-	showDownloadControl: false,
-	downloadLink: '',
-	showPlaybackRateControl: false,
-	changePlaybackRate: () => { },
-	playbackRate: 1,
-	showLoopControl: false,
-	changeLooping: () => { },
-	isLooped: false,
+  showDownloadControl: false,
+  downloadLink: '',
+  showPlaybackRateControl: false,
+  changePlaybackRate: () => { },
+  playbackRate: 1,
+  showLoopControl: false,
+  changeLooping: () => { },
+  isLooped: false,
 };
